387ac91cfb51757cc9460aa1d14ef9ad694c78bf
lelinhtinh <lelinhtinh2013@gmail.com>
2022-10-30 12:28:52 +0700

    refactor: update demo

--- sample/demo.js
+++ sample/demo.js
@@ -1,10 +1,10 @@
-$('.example').catfish({
+jQuery.fn.catfish({
   catfish_src: 'https://i.imgur.com/uNjSy6e.gif',
   catfish_click: 'https://blogtruyen.com/',
   width: 500,
   height: 300,
   mobile_only: false,
-  status: true,
-  limit: -1, // -1 to disable limit
-  expires: 24,
+  status: true, // save closed state
+  limit: 3, // -1 to disable limit
+  expires: 24, // hours
 });
